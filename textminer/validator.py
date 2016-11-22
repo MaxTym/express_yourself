d9de272cf52856c23c292e4d10cd11b1907b783c
Maksym Tymtsiv <mtymtsiv@gmail.com>
2016-11-22 09:11:59 -0500

    Update validator.py

--- textminer/validator.py
+++ textminer/validator.py
@@ -47,22 +47,3 @@ def email(test):
 
 def address(test):
     return re.search(r"^\d+([\w\s]+) [\.\,]? [\w\s]? (\d+)?, ([A-Z]{2}) (\d{5})(\-\d{4})?$", test)
-
-
-
-# """This must be a full address with street number, street, city, state,
-# and ZIP code. Again, US-only."""
-# assert v.address("""368 Agness Harbor
-# Port Mariah, MS 63293""")
-# assert v.address("""96762 Juluis Road Suite 392
-# Lake Imogenemouth, AK 20211""")
-# assert v.address("""671 Tawnya Island Apt. 526
-# Clementeburgh, AK 82652""")
-# assert v.address("""568 Eunice Shoals
-# Parishaven, AK 09922-2288""")
-# assert v.address("8264 Schamberger Spring, Jordanside, MT 98833-0997")
-#
-# assert not v.address("")
-# assert not v.address("99132 Kaylah Union Suite 301")
-# assert not v.address("Lake Joellville, NH")
-# assert not v.address("35981")
